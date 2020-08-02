95b4f40716adf45c2c0d9dbe8a5a091f96ffc449
hemanth4sure <nameishemanthraj@gmail.com>
2020-08-02 12:44:58 +0530

    Adding framework with 3 test cases first time

---
+++ pages/productsPage.js
@@ -0,0 +1,25 @@
+const productDetailPage = require('./productDetailPage.js')
+class productsPage {
+    get productsList(){
+        return $$("//a[@class='product_img_link']")   
+    }    
+    
+    
+    //action
+    
+    clickOnProduct(productId){
+        productId="id_product="+productId;
+        for(let i=0;i<=this.productsList.length-1;i++) {
+            const href = this.productsList[i].getAttribute("href");
+            if(href.includes(productId)){
+                this.productsList[i].click();
+                break;
+            }
+        }
+        browser.pause(8000);
+        browser.switchToFrame(0);
+        return productDetailPage;
+    }
+}
+
+module.exports = new productsPage();
